6d42e5ea4b2d6f5377831a30919ee1f943e55f34
Cianenry Danan <cianenryd@gmail.com>
2022-09-29 20:42:16 -0700

    Lab-08 finsihed

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,17 +5,19 @@ import Container from 'react-bootstrap/Container';
 class Weather extends React.Component {
   render() {
     return (
-      <>
+      <>    
 			<Container>
-        <h2>Weather Forecast</h2>
+      <h2>Weather Forecast</h2>
         {this.props.weatherData.map((forecast, idx) => {
           return (
+            <>
             <Accordion defaultActiveKey='0' flush>
               <Accordion.Item eventKey={idx} >
                 <Accordion.Header>{forecast.date}</Accordion.Header>
                 <Accordion.Body>{forecast.description}</Accordion.Body>
               </Accordion.Item>
             </Accordion>
+            </>
           );
         })}
 				</Container>
